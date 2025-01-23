8b6146e95c99a1cd6fca34f1c28b3b19742a73c2
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-01-23 21:43:00 +0700

    add math class

---
+++ src/classes/MathApp.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(1000,1010);
+        System.out.println(min);
+
+        var max = Math.max(1000,1010);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+
+        System.out.println(Math.floor(10.6));
+    }
+}
